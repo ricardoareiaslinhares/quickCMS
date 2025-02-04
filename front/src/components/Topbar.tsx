dba57b70aea49a79bf6e6b726b9c5ea48bacb3fc
ricardoareiaslinhares <ricardolinhares.dev@gmail.com>
2025-02-04 20:41:41 +0000

    feat:TopBar - diveded in two

--- front/src/components/Topbar.tsx
+++ front/src/components/Topbar.tsx
@@ -4,11 +4,14 @@ type Props = {}
 
 const Topbar = (props: Props) => {
   return (
-    <div className='bg-red-300 min-h-12 max-h-16 flex flex-1 flex-row justify-between px-3'>
-        <div>1</div>
-        <div>2</div>
-        <div>3</div>
+    <div className='flex flex-1 flex-col bg-red-300 min-h-22 max-h-26 '>
+        <div className='bg-red-500 min-h-11'>Topbar</div>
+        <div className=' flex flex-1 flex-row justify-between px-3'>
+            <div>1</div>
+            <div>2</div>
+            <div>3</div>
 
+        </div>
     </div>
   )
 }
